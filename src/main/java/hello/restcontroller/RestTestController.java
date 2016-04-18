b83d0ae6e036d351b2da3e89efa47e7d72df629c
unknown <YessicaCrisent@.mh.gob.sv>
2016-04-18 16:50:42 -0600

    last

--- src/main/java/hello/restcontroller/RestTestController.java
+++ src/main/java/hello/restcontroller/RestTestController.java
@@ -1,7 +1,9 @@
 package hello.restcontroller;
 
 import hello.controllers.*;
+import hello.models.Customer;
 import hello.models.Greeting;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -18,6 +20,14 @@ public class RestTestController {
     @RequestMapping("/greeting2")
     public Greeting greeting(@RequestParam(value="name", required=false, defaultValue="World") String name) {
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+    
+    @RequestMapping("/new_customer")
+    public Customer newCustomer(@RequestParam Map<String,String> requestParams) {
+        String firstName=requestParams.get("firstName");
+        String lastName=requestParams.get("lastName");
+        Customer nc = new Customer(firstName, lastName);
+        return nc;
     }
     
 }
